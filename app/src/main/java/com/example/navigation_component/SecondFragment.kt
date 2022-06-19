4ed28d728e945073b6a885e89d0078e48e2a2581
Mohamed Elgohary <87489620+MohamedElgohary88@users.noreply.github.com>
2022-06-19 18:37:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/navigation_component/SecondFragment.kt
@@ -0,0 +1,25 @@
+package com.example.navigation_component
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+
+class SecondFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_second, container, false)
+
+        view.setOnClickListener {
+            findNavController().navigate(R.id.action_secondFragment_to_firstFragment)
+        }
+
+        return view
+    }
+}
